86bf431a880e5464392e3ba1bfb5626e2b41f2cd
dtaipov <dtaipov@gmail.com>
2018-12-28 17:37:19 +0300

    sign method implemented

--- test/test.js
+++ test/test.js
@@ -25,9 +25,10 @@ const transactionParams = {
 }
 
 describe("Create and sign", () => {
-  let key, resKch, resWeb3;
-  const keyname = 'test1@76de427d42c38be4';  
+  let key, signResKch, signResWeb3, resKch, resWeb3;
+  const keyname = 'test1@76de427d42c38be4';
   const privateKey = '0xb3d3427eea7867c243baaf2f4c67a9551eea2ea96556acfb0051dffa18d182d4';
+  const message = '12345';
 
   it('Create new key in Keychain', async () => {
     const keyInstance = await Module.Keychain.create();
@@ -36,6 +37,29 @@ describe("Create and sign", () => {
     await keyInstance.term();
     expect(key).to.have.string('@');
   });
+  //
+  // it('Sign with web3', async () => {
+  //   signResWeb3 = await web3.eth.accounts.sign(message, privateKey);
+  //   console.log('signResWeb3: ', signResWeb3);
+  //   expect(signResWeb3).to.have.property('message');
+  // });
+  //
+  // it('Sign with overriden web3', async () => {
+  //   Module.override(web3);
+  //   signResKch = await web3.eth.accounts.sign(message, keyname);
+  //   console.log('signResKch: ', signResKch);
+  //   expect(signResKch).to.have.property('signature');
+  // });
+  //
+  // it('Overriden web3 sign valid', async () => {
+  //   expect(signResKch.message).to.equal(signResWeb3.message);
+  //   expect(signResKch.messageHash).to.equal(signResWeb3.messageHash);
+  //   expect(signResKch.v).to.equal(signResWeb3.v);
+  //   expect(signResKch.r).to.equal(signResWeb3.r);
+  //   expect(signResKch.s).to.equal(signResWeb3.s);
+  //   expect(signResKch.signature.substr(0, signResKch.signature.length-2)).to.
+  //   equal(signResWeb3.signature.substr(0, signResWeb3.signature.length-2));  // trunc V for now because keychain gives different from web3 V
+  // });
 
   it('Sign transaction with web3', async () => {
     resWeb3 = await web3.eth.accounts.signTransaction(transactionParams, privateKey);
