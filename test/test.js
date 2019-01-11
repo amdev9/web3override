e00fe32670e69ab5c83b0d37fcc9839f078d6cff
dtaipov <dtaipov@gmail.com>
2019-01-11 13:27:47 +0300

    override in require without extra code

--- test/test.js
+++ test/test.js
@@ -1,10 +1,9 @@
 var expect = require('chai').expect;
 const Web3 = require('web3');
- 
-const Module = require('../lib/index');
+
 const API_KEY = 'https://ropsten.infura.io/v3/046804e3dd3240b09834531326f310cf';
 
-let web3 = new Web3(new Web3.providers.HttpProvider(API_KEY)); 
+let web3 = new Web3(new Web3.providers.HttpProvider(API_KEY));
 
 const to = '0xE8899BA12578d60e4D0683a596EDaCbC85eC18CC';
 const value = 100;
@@ -22,21 +21,21 @@ const transactionParams = {
   data,
   gas,
   chainId
-}
+};
 
 describe("Create and sign", () => {
-  let key, signResKch, signResWeb3, resKch, resWeb3;
+  let key, signResKch, createResKch, signResWeb3, resKch, resWeb3;
   const keyname = 'test1@76de427d42c38be4';
   const privateKey = '0xb3d3427eea7867c243baaf2f4c67a9551eea2ea96556acfb0051dffa18d182d4';
   const message = '12345';
 
-  it('Create new key in Keychain', async () => {
-    const keyInstance = await Module.Keychain.create();
-    const data = await keyInstance.createKey('test1');
-    key = data.result;
-    await keyInstance.term();
-    expect(key).to.have.string('@');
-  });
+  // it('Create new key in Keychain', async () => {
+  //   const keyInstance = await Module.Keychain.create();
+  //   const data = await keyInstance.createKey('test1');
+  //   key = data.result;
+  //   await keyInstance.term();
+  //   expect(key).to.have.string('@');
+  // });
 
   it('Sign with web3', async () => {
     signResWeb3 = await web3.eth.accounts.sign(message, privateKey);
@@ -50,7 +49,7 @@ describe("Create and sign", () => {
   });
 
   it('Sign with overriden web3', async () => {
-    Module.override(web3);
+    require('../lib/index')(web3);
     signResKch = await web3.eth.accounts.sign(message, keyname);
     console.log('signResKch: ', signResKch);
     expect(signResKch).to.have.property('signature');
